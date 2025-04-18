52f66f9d28e5df496a7284b696cbbf71656e8c1e
moronturtle <hilmangp@gmail.com>
2025-04-18 07:20:11 +0700

    fix error eslint

--- app/services/redis_client.py
+++ app/services/redis_client.py
@@ -3,6 +3,6 @@ import os
 import redis
 
 redis_host = os.env("REDIS_HOST", "localhost")
-redis_port = int(os.env("REDIS_PORT",123))
+redis_port = int(os.env("REDIS_PORT", 123))
 
-redis_client = redis.Redis(host = redis_host , port=redis_port ,decode_responses= True)
+redis_client = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
